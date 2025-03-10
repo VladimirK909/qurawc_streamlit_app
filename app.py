c3b10eafedec76ee7298ce8e2eb0700c033d07bc
Vladimir Kitanovski <vladimirkitanovski909@gmail.com>
2025-03-10 03:14:34 +0100

    fixed text and params

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def get_image_files(folder_path):
 def main():
     st.title("QuRAWC Visualizer App")
 
+    st.write("Use the slider below to cycle through the firefighting steps and observe how the fire spreads and is controlled.")
+
     folder_path = "gif1"  # Change this to the path of your images folder
 
     if not os.path.exists(folder_path):
@@ -24,12 +26,12 @@ def main():
 
     image_files.sort()  # Sort files alphabetically
 
-    index = st.slider("Select an image", 0, len(image_files) - 1, 0)
+    index = st.slider("Visualize the firefighting animation shown in the presentation:", 0, len(image_files) - 1, 0)
 
     image_path = os.path.join(folder_path, image_files[index])
     image = Image.open(image_path)
 
-    st.image(image, caption=image_files[index], use_column_width=True)
+    st.image(image, caption=image_files[index], use_container_width=True)
 
 if __name__ == "__main__":
     main()
